9d3b45606c3bac9081ac4ac33cfa398bd05de9c1
secret <secretx500@outlook.com>
2025-04-08 22:21:11 +0800

    fix: update App.vue to use TypeScript in script setup

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import TakePhoto from "./TakePhoto.vue";
 import AudioRecorder from "./AudioRecorder.vue";
 import CameraRecorder from "./CameraRecorder.vue";
