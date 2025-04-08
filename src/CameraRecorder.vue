95db68cbe3e00fdf68dad6e6b281d1baa53c8e9c
secret <secretx500@outlook.com>
2025-04-08 22:44:04 +0800

    fix: remove mirror class from videos

--- src/CameraRecorder.vue
+++ src/CameraRecorder.vue
@@ -5,9 +5,8 @@
       <button class="px-4 py-2 mr-2 bg-blue-500 text-white" @click="handleStop">结束录摄像头</button>
     </div>
     <div class="flex items-center">
-      <!-- 注意镜像效果 scale-x-[-1] -->
-      <video ref="previewDomRef" class="w-1/2 bg-black scale-x-[-1]"></video>
-      <video ref="resultDomRef" class="w-1/2 scale-x-[-1]" controls="true"></video>
+      <video ref="previewDomRef" class="w-1/2 bg-black"></video>
+      <video ref="resultDomRef" class="w-1/2" controls="true"></video>
     </div>
   </div>
 </template>
@@ -15,7 +14,6 @@
 <script lang="ts" setup>
 import { useCameraRecorder } from "./recorder";
 
-// 录屏幕
 const { start, stop, previewDomRef, resultDomRef, resultBlob } = useCameraRecorder();
 
 async function handleStart() {
