ad305ea50b8a5a7c63937c17c935e7a143d1181a
secret <secretx500@qq.com>
2026-07-15 16:37:35 +0800

    refactor: Update base path to root

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwind from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/media-devices-demo/",
+  base: "/",
   plugins: [vue(), tailwind()],
 
   resolve: {
